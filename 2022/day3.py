99aa2300a46c330f7a7e00bb2048dd0e6c699f3c
cpcf <piracyonmars@gmail.com>
2022-12-04 11:10:55 +0000

    day3

---
+++ 2022/day3.py
@@ -0,0 +1,19 @@
+PRIORITY = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def part1(input):
+  total = 0
+  for line in input:
+    first_compartment, second_compartment = line[:len(line)//2], line[len(line)//2:]
+    total = total + PRIORITY.index(''.join(set(first_compartment).intersection(second_compartment)))
+  return total
+
+def part2(input):
+  total = 0
+  groups = [input[i * 3:(i + 1) * 3] for i in range((len(input) + 3 - 1) // 3 )]
+  for group in groups:
+      total = total + PRIORITY.index(''.join(set(''.join(set(group[1]).intersection(group[2]))).intersection(group[0])))
+  return total
+
+input = open("input.txt", "r").read().splitlines()
+print("Part1: " + str(part1(input)))
+print("Part2: " + str(part2(input)))

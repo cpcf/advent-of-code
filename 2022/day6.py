6eaa16278ef1fdd2ce5d478c8c60051798fced06
C <6714828+cpcf@users.noreply.github.com>
2022-12-06 13:51:16 +0000

    Update day6.py, better function name

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,10 +1,10 @@
 import sys
 
-def find_start(input, window_size):
+def find_unique_window(input, window_size):
   for i in range(len(input) - window_size + 1):
     if len(set(input[i: i + window_size])) == window_size:
       return i + window_size
 
 input = open(sys.argv[1], "r").read()
-print("Part 1: " + str(find_start(input, 4)))
-print("Part 1: " + str(find_start(input, 12)))
+print("Part 1: " + str(find_unique_window(input, 4)))
+print("Part 1: " + str(find_unique_window(input, 12)))
